ef6dc7dfa6a295e5cd8ec8f3481e8cfbdee6ec55
RohitBajra <104704874+rohitnewgit@users.noreply.github.com>
2023-08-25 19:24:48 +0545

    Login Page created

---
+++ lib/users/authentication/login_screen.dart
@@ -0,0 +1,276 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  var formKey = GlobalKey<FormState>();
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+  var isObsecure = true.obs;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    //login screen header
+                    Container(
+                      width: Get.width,
+                      height: Get.height * .3,
+                      child: Image.asset("images/login.jpg"),
+                    ),
+                    //sign-in form
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Container(
+                        height: Get.height * .8,
+                        decoration: const BoxDecoration(
+                          color: Colors.white24,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(60),
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 8,
+                              color: Colors.black26,
+                              offset: Offset(0, -3),
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(30, 30, 30, 8.0),
+                          child: Column(
+                            children: [
+                              //login form
+                              Form(
+                                key: formKey,
+                                child: Column(
+                                  children: [
+                                    //email field
+                                    TextFormField(
+                                      controller: emailController,
+                                      validator: (value) => value == ''
+                                          ? "Please enter email"
+                                          : null,
+                                      decoration: InputDecoration(
+                                        prefixIcon: const Icon(Icons.email),
+                                        iconColor: Colors.black,
+                                        hintText: "Email",
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30),
+                                          borderSide: const BorderSide(
+                                            color: Colors.white60,
+                                          ),
+                                        ),
+                                        enabledBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30),
+                                          borderSide: const BorderSide(
+                                            color: Colors.white60,
+                                          ),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30),
+                                          borderSide: const BorderSide(
+                                            color: Colors.white60,
+                                          ),
+                                        ),
+                                        disabledBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30),
+                                          borderSide: const BorderSide(
+                                            color: Colors.white60,
+                                          ),
+                                        ),
+                                        contentPadding:
+                                            const EdgeInsets.symmetric(
+                                          horizontal: 14,
+                                          vertical: 6,
+                                        ),
+                                        fillColor: Colors.white,
+                                        filled: true,
+                                      ),
+                                    ),
+
+                                    const SizedBox(height: 18),
+                                    //password field
+                                    Obx(
+                                      () => TextFormField(
+                                        obscureText: isObsecure.value,
+                                        controller: passwordController,
+                                        validator: (value) => value == ''
+                                            ? "Please enter password"
+                                            : null,
+                                        decoration: InputDecoration(
+                                          prefixIcon:
+                                              const Icon(Icons.vpn_key_sharp),
+                                          suffixIcon: Obx(
+                                            () => GestureDetector(
+                                              onTap: () {
+                                                isObsecure.value =
+                                                    !isObsecure.value;
+                                              },
+                                              child: Icon(
+                                                isObsecure.value
+                                                    ? Icons.visibility_off
+                                                    : Icons.visibility,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                          ),
+                                          iconColor: Colors.black,
+                                          hintText: "Password",
+                                          border: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(30),
+                                            borderSide: const BorderSide(
+                                              color: Colors.white60,
+                                            ),
+                                          ),
+                                          enabledBorder: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(30),
+                                            borderSide: const BorderSide(
+                                              color: Colors.white60,
+                                            ),
+                                          ),
+                                          focusedBorder: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(30),
+                                            borderSide: const BorderSide(
+                                              color: Colors.white60,
+                                            ),
+                                          ),
+                                          disabledBorder: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(30),
+                                            borderSide: const BorderSide(
+                                              color: Colors.white60,
+                                            ),
+                                          ),
+                                          contentPadding:
+                                              const EdgeInsets.symmetric(
+                                            horizontal: 14,
+                                            vertical: 6,
+                                          ),
+                                          fillColor: Colors.white,
+                                          filled: true,
+                                        ),
+                                      ),
+                                    ),
+
+                                    const SizedBox(height: 18),
+                                    //login button
+                                    Material(
+                                      color: Colors.black,
+                                      borderRadius: BorderRadius.circular(30),
+                                      child: InkWell(
+                                        onTap: () {},
+                                        borderRadius: BorderRadius.circular(30),
+                                        child: const Padding(
+                                          padding: EdgeInsets.symmetric(
+                                            vertical: 10,
+                                            horizontal: 28,
+                                          ),
+                                          child: Text(
+                                            "Login",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 16,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+
+                              const SizedBox(height: 16),
+                              //"dont have an account"-Text and signup-TextButton
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Text(
+                                    "Don't have an Account?",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {},
+                                    child: const Text(
+                                      "Sign up",
+                                      style: TextStyle(
+                                        color: Colors.red,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+
+                              const Text(
+                                "Or",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 16,
+                                ),
+                              ),
+
+                              //"Are you an Admin"-Text and signup-TextButton
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Text(
+                                    "Are you an Admin?",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {},
+                                    child: const Text(
+                                      "Sign in as Admin",
+                                      style: TextStyle(
+                                        color: Colors.red,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
